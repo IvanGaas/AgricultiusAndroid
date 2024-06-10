a0d04486797d130e62d2d9607db8d27fc858692a
IvanGaas <ivan.garcia.astudillo@estudiantat.upc.edu>
2024-06-10 17:14:23 +0200

    Versió2

--- app/src/main/java/com/example/agricultius/Login.java
+++ app/src/main/java/com/example/agricultius/Login.java
@@ -5,11 +5,26 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class Login extends AppCompatActivity {
 
@@ -44,19 +59,73 @@ public class Login extends AppCompatActivity {
     private void authenticateUser(final String username, final String password) {
         // Crear un nuevo Thread para la autenticación
         new Thread(new Runnable() {
+            InputStream stream = null;
+            String result = null;
+            Handler handler = new Handler();
             @Override
             public void run() {
                 // Simular una operación de autenticación (por ejemplo, verificar con un servidor)
                 boolean success = false;
                 try {
                     // Simular un tiempo de espera para la autenticación
-                    Thread.sleep(2000);
+                   // Thread.sleep(2000);
                     // Verificación de credenciales (esto es solo un ejemplo básico)
-                    if (username.equals("user") && password.equals("password")) {
-                        success = true;
+                   // if (username.equals("user") && password.equals("password")) {
+                     //   success = true;
+                 //   }
+                    String query = String.format("http://localhost:9000/Application/Login");
+                    URL url = new URL(query);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setReadTimeout(10000 );
+                    conn.setConnectTimeout(15000 /* milliseconds */);
+                    conn.setRequestMethod("POST");
+                    conn.setDoInput(true);
+                    conn.setDoOutput(true);
+                    conn.connect();
+
+
+                    // enviar paràmetres de la funció Login en el cos del missatge
+
+                    String params = "username="+username + "&password="+ password;
+                    Log.i("serverTest ", params);
+
+                    OutputStream os = conn.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(
+                            new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(params.toString());
+
+                    writer.flush();
+                    writer.close();
+                    os.close();
+
+                    Log.i("serverTest ", "esperant resposta");
+
+                    //obtenir resposta del servidor
+                    stream = conn.getInputStream();
+                    BufferedReader reader = null;
+                    StringBuilder sb = new StringBuilder();
+                    reader = new BufferedReader(new InputStreamReader(stream));
+                    String line = null;
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
                     }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    result = sb.toString();
+                    conn.disconnect();
+                    // Mostrar resultat en el quadre de text.
+                    // Codi incorrecte
+                    // EditText n = (EditText) findViewById (R.id.edit_message);
+                    //n.setText(result);
+
+                    //Codi correcte
+                    Log.i("serverTest", result);
+                    handler.post(new Runnable() {
+                        public void run() {
+                            TextView n = (TextView) findViewById (R.id.main);
+                            n.setText(result);
+                        }
+                    });
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
 
                 final boolean finalSuccess = success;
--- app/src/main/java/com/example/agricultius/MainActivity.java
+++ app/src/main/java/com/example/agricultius/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), AnActivity.class);
+                Intent intent = new Intent(view.getContext(), Login.class);
                 view.getContext().startActivity(intent);}
         });
 
@@ -39,131 +39,9 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), AnotherActivity.class);
+                Intent intent = new Intent(view.getContext(), Register.class);
                 view.getContext().startActivity(intent);}
         });
-    }
-    public void connectServerGET(View view) {
-
-        new Thread(new Runnable() {
-            InputStream stream = null;
-            String str = "";
-            String result = null;
-            Handler handler = new Handler();
-            public void run() {
-
-                try {
-                    String query = String.format("http://localhost:9000/Application/Agricultius");
-                    URL url = new URL(query);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setReadTimeout(10000 );
-                    conn.setConnectTimeout(15000 /* milliseconds */);
-                    conn.setRequestMethod("GET");
-                    conn.setDoInput(true);
-                    conn.connect();
-                    stream = conn.getInputStream();
-
-                    BufferedReader reader = null;
-
-                    StringBuilder sb = new StringBuilder();
-
-                    reader = new BufferedReader(new InputStreamReader(stream));
-
-                    String line = null;
-                    while ((line = reader.readLine()) != null) {
-                        sb.append(line);
-                    }
-                    result = sb.toString();
-                    conn.disconnect();
-                    // Mostrar resultat en el quadre de text.
-                    // Codi incorrecte
-                    // EditText n = (EditText) findViewById (R.id.edit_message);
-                    //n.setText(result);
-
-                    //Codi correcte
-                    Log.i("serverTest", result);
-                    handler.post(new Runnable() {
-                        public void run() {
-                            TextView n = (TextView) findViewById (R.id.main);
-                            n.setText(result);
-                        }
-                    });
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-    }
-
-    public void connectServerPOST(View view) {
-
-        new Thread(new Runnable() {
-            InputStream stream = null;
-            OutputStream streamOut = null;
-            String str = "";
-            String result = null;
-            Handler handler = new Handler();
-            public void run() {
-
-                try {
-                    String query = String.format("http://localhost:9000/Application/Login");
-                    URL url = new URL(query);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setReadTimeout(10000 );
-                    conn.setConnectTimeout(15000 /* milliseconds */);
-                    conn.setRequestMethod("POST");
-                    conn.setDoInput(true);
-                    conn.setDoOutput(true);
-                    conn.connect();
-
-
-                    // enviar paràmetres de la funció Login en el cos del missatge
-
-                    String params = "username=ivan&password=1234";
-                    Log.i("serverTest ", params);
-
-                    OutputStream os = conn.getOutputStream();
-                    BufferedWriter writer = new BufferedWriter(
-                            new OutputStreamWriter(os, "UTF-8"));
-                    writer.write(params.toString());
-
-                    writer.flush();
-                    writer.close();
-                    os.close();
-
-                    Log.i("serverTest ", "esperant resposta");
-
-                    //obtenir resposta del servidor
-                    stream = conn.getInputStream();
-                    BufferedReader reader = null;
-                    StringBuilder sb = new StringBuilder();
-                    reader = new BufferedReader(new InputStreamReader(stream));
-                    String line = null;
-                    while ((line = reader.readLine()) != null) {
-                        sb.append(line);
-                    }
-                    result = sb.toString();
-                    conn.disconnect();
-                    // Mostrar resultat en el quadre de text.
-                    // Codi incorrecte
-                    // EditText n = (EditText) findViewById (R.id.edit_message);
-                    //n.setText(result);
-
-                    //Codi correcte
-                    Log.i("serverTest", result);
-                    handler.post(new Runnable() {
-                        public void run() {
-                            TextView n = (TextView) findViewById (R.id.main);
-                            n.setText(result);
-                        }
-                    });
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
     }
 }
 
